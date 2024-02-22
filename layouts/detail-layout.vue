37628a86420f2b17eea647b0c94c52a2cc63169f
Alan Yuan <a@ldwid.com>
2024-02-22 07:34:25 +0000

    fix: empty component issue

--- layouts/detail-layout.vue
+++ layouts/detail-layout.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h1>detail layout</h1>
+    <v-breadcrumbs :items="['首页 Home', '应用 Apps', '字段拼接器 Field-Splicer']">
+      <template v-slot:prepend>
+        <v-icon size="small" icon="$vuetify"></v-icon>
+      </template>
+    </v-breadcrumbs>
     <slot />
   </div>
 </template>
