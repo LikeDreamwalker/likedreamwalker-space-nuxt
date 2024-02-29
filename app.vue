ea302094a4e49314cf7ac07b29cf4943a5482101
Alan Yuan <a@ldwid.com>
2024-03-01 01:11:20 +0800

    feat: basic background support

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
   <v-theme-provider :theme="theme">
     <NuxtLayout class="likedreamwalker-space">
+      <!-- <div class="animation-container">
+        <svg v-for="circle in circles" :key="circle.id" class="circle-animation" :style="circle.style"
+          viewBox="0 0 100 100">
+          <circle cx="50" cy="50" r="45" fill="#0066ff" />
+        </svg>
+      </div> -->
+
       <v-defaults-provider :defaults="{ VApp: { style: 'font-family: Raleway, sans-serif;font-weight: 400;' } }">
         <v-app>
           <NuxtPage />
@@ -12,7 +19,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-
+const circles = ref([]);
 const theme = ref('light')
 
 let mql
@@ -28,6 +35,17 @@ onMounted(() => {
     theme.value = mql.matches
       ? 'dark'
       : 'light'
+  }
+  for (let i = 0; i < 10; i++) {
+    circles.value.push({
+      id: i,
+      style: {
+        top: `${Math.random() * 100}%`,
+        left: `${Math.random() * 100}%`,
+        animationDuration: `${Math.random() * 5 + 1}s`,
+        animationDelay: `${Math.random() * 2}s`,
+      },
+    });
   }
 })
 
@@ -45,6 +63,38 @@ onUnmounted(() => {
 <style lang="scss" scoped>
 .likedreamwalker-space {
   height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+
+  .animation-container {
+    position: fixed;
+    overflow: hidden;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+
+    .circle-animation {
+      position: absolute;
+      width: 200px;
+      height: 200px;
+      animation: move 5s infinite linear;
+    }
+
+
+  }
+}
+
+@keyframes move {
+  0% {
+    transform: translate(-50%, -50%) scale(0);
+    opacity: 1;
+  }
+
+  100% {
+    transform: translate(150%, 150%) scale(1);
+    opacity: 0;
+  }
 }
 </style>
 
