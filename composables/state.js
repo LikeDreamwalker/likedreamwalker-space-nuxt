a1902001e9c72643ee5c032e721fd213ed0d1dbb
Alan Yuan <a@ldwid.com>
2024-03-01 00:57:38 +0800

    feat: basic menu support

--- composables/state.js
+++ composables/state.js
@@ -1,21 +1,3 @@
 export const useGlobalState = () => {
-  return useState("globalState", () => ({
-    menu: [
-      {
-        renderTitleName: "应用",
-        renderTitleText: "APPS",
-        to: "/apps",
-      },
-      {
-        renderTitleName: "故事",
-        renderTitleText: "STORIES",
-        to: "https://stories.ldwid.com/",
-      },
-      {
-        renderTitleName: "相册",
-        renderTitleText: "STORIES",
-        to: "https://stories.ldwid.com/",
-      },
-    ],
-  }));
+  return useState("globalState", () => ({}));
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="index-container d-flex justify-center align-center align-content-center flex-wrap line">
-    <v-row>
+    <v-row class="index-container__topic">
       <v-col class="index-container__title d-flex justify-center align-center pb-0" cols="12"
         style="font-size: 3rem;">RETURN</v-col>
       <v-col class="index-container__subtitle d-flex justify-center align-center pt-0" cols="12">By
         LikeDreamwalker</v-col>
     </v-row>
+    <!-- <v-row class="index-container__menu">
+      <v-col class="index-container__menu-item d-flex justify-center align-baseline" cols="12" v-for="item, index in menu"
+        :key="index">
+        <span class="index-container__menu-item--chn"> {{ item.renderTitleName }}</span>
+        <span class="index-container__menu-item--eng"> / {{ item.renderTitleText }}</span>
+      </v-col>
+    </v-row> -->
   </div>
 </template>
 <script setup>
@@ -14,7 +21,23 @@ definePageMeta({
   renderTitleName: 'LDWINDEX',
   keepalive: true
 })
-const state = useGlobalState()
+const menu = ref([
+  {
+    renderTitleName: "应用",
+    renderTitleText: "APPS",
+    to: "/apps",
+  },
+  {
+    renderTitleName: "故事",
+    renderTitleText: "STORIES",
+    to: "https://stories.ldwid.com/",
+  },
+  {
+    renderTitleName: "相册",
+    renderTitleText: "STORIES",
+    to: "https://stories.ldwid.com/",
+  },
+])
 </script>
 <style lang="scss" scoped>
 .index-container {
@@ -31,6 +54,20 @@ const state = useGlobalState()
     font-weight: 400;
     font-family: "Cormorant Garamond", serif;
     font-style: italic;
+  }
+
+  &__menu-item {
+
+
+    &--chn {
+      font-size: 2rem;
+      font-weight: 700;
+    }
+
+    &--eng {
+      font-size: 1.5rem;
+      font-weight: 400;
+    }
   }
 }
 </style>
